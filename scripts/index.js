9dc3ef384f498652d7889c5f2983d2a79976da43
AlejaRairan <alejandra.0219@hotmail.com>
2025-09-18 18:53:23 -0500

    adding parts of sprint 8

--- scripts/index.js
+++ scripts/index.js
@@ -8,32 +8,6 @@ const nameForm = document.querySelector("#name");
 const aboutForm = document.querySelector("#about");
 const iLike = document.querySelector("#corazon");
 
-/* const initialCards = [
-  {
-    name: "Vale de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-  },
-  {
-    name: "Montanhas Carecas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-  },
-  {
-    name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-  },
-  */
 
 icon.addEventListener("click", function() {
     form.classList.add("form-open");
@@ -52,20 +26,108 @@ save.addEventListener("click", (e) => {
   form.classList.remove("form-open");  
 });
 
-
-iLike.addEventListener("click", function() {
-  iLike.classList.toggle("grid__icon-active");
-});
-
 nameForm.value = headerSubtitle.textContent;
 aboutForm.value = headerDescription.textContent;
 
-/** *
-const gridTemplate = document
-.querySelector("#grid-template");
-.querySelector(".grid");
+const add = document.querySelector("#addButton");
+const secondForm = document.querySelector("#formAdd");
+const closeForm = document.querySelector("#closeSecondForm");
+const saveSecondForm = document.querySelector("#saveSecondForm");
+const nameSecondForm = document.querySelector("#title");
+const linkSecondForm = document.querySelector("#link");
+
+add.addEventListener("click", function() {
+  secondForm.classList.add("form-open");
+});
+
+closeForm.addEventListener("click", function() {
+  console.log("click");
+  secondForm.classList.remove("form-open");
+});
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
+const Title = document.querySelector(".grid__title");
+const Image = document.querySelector(".grid__image");
+const likeIcon = document.querySelector(".grid__icon");
+const gridContainer = document.querySelector(".grid__section");
+const gridDelete = document.querySelectorAll(".grid__delete-icon");
+const gridTitle = document.querySelectorAll(".grid__title");
 
 
-const getGridElement = (data) => {
-  const GridElement = 
-  */ 
+function addCard(gridImage, gridTitle) {
+const gridTemplate = document.querySelector("#gridTemplate").content;
+const gridElement = gridTemplate.querySelector(".grid__card").cloneNode(true);
+  gridElement.querySelector(".grid__image").src = gridImage;
+  gridElement.querySelector(".grid__title").textContent = gridTitle;
+  gridElement.querySelector(".grid__image").alt = gridTitle;
+  gridElement.querySelector(".grid__icon").addEventListener("click", function(evt) {
+    evt.target.classList.toggle("grid__icon-active");
+    
+  });
+  gridElement.querySelector(".grid__delete-icon").addEventListener("click", function(evt) {
+    const card = evt.target.closest(".grid__card");
+    if (card) card.remove();
+  });
+  gridContainer.prepend(gridElement);
+}
+
+initialCards.forEach(function(card) {
+  addCard(card.link, card.name);
+});
+saveSecondForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(linkSecondForm);
+  console.log(linkSecondForm.value);
+  addCard(linkSecondForm.value, nameSecondForm.value);
+  linkSecondForm.value = "";
+  nameSecondForm.value = "";
+  secondForm.classList.remove("form-open");
+});
+
+const images = document.querySelectorAll(".grid__image");
+const modal = document.querySelector("#imageModal");
+const modalImg = document.querySelector('#modalImg');
+const modalCaption = document.querySelector('#modalCaption');
+const closeBtn = document.querySelector('#closeModal');
+
+images.forEach((img) => {
+  img.addEventListener("click", (e) => {
+  modal.style.display = 'flex';
+  modalImg.src = img.src;
+  modalCaption.textContent = img.alt;
+});
+});
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
